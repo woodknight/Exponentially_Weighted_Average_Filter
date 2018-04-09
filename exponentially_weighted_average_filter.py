0897d5b1e427b69b4acccd575a65471e2f15ac40
J <lijiangpku@gmail.com>
2018-04-09 11:15:48 -0700

    implement one-sided median filter

--- exponentially_weighted_average_filter.py
+++ exponentially_weighted_average_filter.py
@@ -5,21 +5,42 @@ Created on Sun Apr  8 22:16:57 2018
 """
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.signal import medfilt
+#from scipy.signal import medfilt
 
 #%% Generage data
-N = 3000
+N = 10000
+w = 501 # median filter window size
+
 sigma = 0.1
-data = np.random.random(N)*sigma
-data[0] = sigma # make the first point biased
+data = np.random.random(N)*sigma + 1
+#data[0] += sigma # make the first point biased
 
 break_idx = int(N/2)
-data[break_idx] = -sigma
+data[break_idx] += -sigma
 data[break_idx:] += 1
 
-
+#%% median filter
+def medfilt(data, w):
+    # one-sided median filter for 1-D array
+    # w - windows size, needs to be odd
+    
+    assert(w%2 == 1)
+    L = len(data)
+    data_filtered = np.zeros(L)
+    
+    startp = 0
+    endp = 0
+    for i in range(L):
+        if i - w + 1 < 0:
+            startp = 0
+        else:
+            startp = i - w + 1
+        endp = i+1
+        data_filtered[i] = np.median(data[startp:endp])
+    return data_filtered
+    
 #%% Exponentially weighted average with bias correction
-beta = 0.998
+beta = 1 - 1.0/w
 data_filtered = np.zeros(len(data) + 1)
 data_filtered_corrected = np.copy(data_filtered)
 bias_correction_idx = 0
@@ -34,10 +55,10 @@ for i in range(N):
     # bias correction    
     data_filtered_corrected[i+1] = data_filtered[i+1] / (1 - beta ** (i+1 - bias_correction_idx))
 
-data_median = medfilt(data, int(1/(1-beta)))
+data_median = medfilt(data, w)
 
 plt.plot(data, label = 'raw')
-plt.plot(data_filtered[1:], label = 'filtered')
+#plt.plot(data_filtered[1:], label = 'filtered')
 plt.plot(data_filtered_corrected[1:], label = 'filtered corrected')
 plt.plot(data_median, label = 'median filter')
 plt.legend()
